9ea4571b8cc484f53d42b80824a525526f3f8610
jrnsmisrt <jeroen.smissaert@gmail.com>
2022-01-19 15:00:50 +0100

    BUILD(Login)- added loginForm + validation, added getUsers to userService

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -2,6 +2,8 @@ import {Injectable} from '@angular/core';
 import {environment} from "../../environments/environment";
 import {HttpClient} from "@angular/common/http";
 import {UserModule} from "../user/user.module";
+import {User} from "../user/model/User";
+import {Observable} from "rxjs";
 
 
 @Injectable({
@@ -14,7 +16,12 @@ export class UserService {
     this.userUrl = `${environment.backEndUrl}/users`;
   }
 
+  getUsers(): Observable<any>{
+    return this.http.get<User[]>(this.userUrl);
+  }
 
 
+  login() {
 
+  }
 }
---
+++ src/app/user/model/User.ts
@@ -0,0 +1,9 @@
+export interface User{
+  id: string,
+  firstName: string,
+  lastName: string,
+  password: string,
+  email: string,
+  company: string,
+  userRole: string
+}
