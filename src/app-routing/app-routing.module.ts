5c847dbdac4e92bc48aa42af282a6f5b5d60b2c7
tmassart873 <tim.massart@hotmail.com>
2022-01-24 15:23:10 +0100

    fix(story5): wrong component linked

--- src/app-routing/app-routing.module.ts
+++ src/app-routing/app-routing.module.ts
@@ -4,13 +4,12 @@ import {CreateUserComponent} from "../app/user/create-user/create-user.component
 import {CoacheeDetailComponent} from "../app/user/user-coachee-detail/coachee-detail.component";
 import {HomepageComponent} from "../app/homepage/homepage.component";
 import {UserLoginComponent} from "../app/user/user-login/user-login.component";
-import {UserProfileComponent} from "../app/user/user-profile/user-profile.component";
 
 const routes: Routes=[
   {path: '', component: HomepageComponent},
   {path: 'login', component: UserLoginComponent},
   {path: 'register', component: CreateUserComponent},
-  {path: 'users/:id/profile', component: UserProfileComponent},
+  {path: 'users/:id/profile', component: CoacheeDetailComponent},
   {path: '**', redirectTo: ''}
 ]
 
