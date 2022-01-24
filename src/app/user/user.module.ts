c37ed9ee2fecbc84faec4c34c9c26a7f4d046d36
tmassart873 <tim.massart@hotmail.com>
2022-01-24 15:14:02 +0100

    build(story5): added new component

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -8,6 +8,7 @@ import { CoachDetailComponent } from './user-coach-detail/coach-detail.component
 import { UserLoginComponent } from './user-login/user-login.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import {RouterModule} from "@angular/router";
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 
 
@@ -18,7 +19,8 @@ import {RouterModule} from "@angular/router";
     CoacheeDetailComponent,
     ApplyForCoachComponent,
     CoachDetailComponent,
-    UserLoginComponent
+    UserLoginComponent,
+    UserProfileComponent
   ],
     imports: [
         CommonModule,
