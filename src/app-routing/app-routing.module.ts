b22d0f79bbf807d3cd3029e59015c6523704e6ee
rudykierbel <rudykierbel@gmail.com>
2022-01-25 11:47:22 +0100

    refactor(user-routing): cleaned up files

--- src/app-routing/app-routing.module.ts
+++ src/app-routing/app-routing.module.ts
@@ -1,21 +1,28 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {CreateUserComponent} from "../app/user/create-user/create-user.component";
-import {CoacheeDetailComponent} from "../app/user/user-coachee-detail/coachee-detail.component";
 import {HomepageComponent} from "../app/homepage/homepage.component";
 import {UserLoginComponent} from "../app/user/user-login/user-login.component";
+import {UserRoutingModule} from "../app/user/user-routing/user-routing.module";
+import {CoacheeDetailComponent} from "../app/user/user-coachee-detail/coachee-detail.component";
+import {UserPanelComponent} from "../app/user/user-panel/user-panel.component";
+import {ApplyForCoachComponent} from "../app/user/apply-for-coach/apply-for-coach.component";
 
-const routes: Routes=[
+const routes: Routes = [
   {path: '', component: HomepageComponent},
   {path: 'login', component: UserLoginComponent},
   {path: 'register', component: CreateUserComponent},
-  {path: 'users/:id/profile', component: CoacheeDetailComponent},
+  {path: 'users/:id', component: UserPanelComponent, children: [
+      {path: 'profile', component: CoacheeDetailComponent},
+      {path: 'become-coach', component: ApplyForCoachComponent}
+    ]},
   {path: '**', redirectTo: ''}
 ]
 
 @NgModule({
   declarations: [],
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), UserRoutingModule],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
