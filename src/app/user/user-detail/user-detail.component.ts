287f2dbf61daae6acaeb1b08ec65da4807bac778
Maxim <maximverbeeck@hotmail.com>
2022-01-26 13:56:22 +0100

    build(refactor component) : refactor component + checking if front-end still the same + changed selector

--- src/app/user/user-detail/user-detail.component.ts
+++ src/app/user/user-detail/user-detail.component.ts
@@ -1,14 +1,13 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from "../../service/user.service";
-import {Observable} from "rxjs";
 import {User} from "../model/User";
 
 @Component({
-  selector: 'app-coachee-detail',
-  templateUrl: './coachee-detail.component.html',
-  styleUrls: ['./coachee-detail.component.css']
+  selector: 'app-user-detail',
+  templateUrl: './user-detail.component.html',
+  styleUrls: ['./user-detail.component.css']
 })
-export class CoacheeDetailComponent implements OnInit{
+export class UserDetailComponent implements OnInit{
 
   user! : User | null;
   email!: string | null;
