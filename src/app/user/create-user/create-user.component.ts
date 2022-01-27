38c9ea2b01e716878ad6ee0b2991a27894af37a4
Ben Mees <benmees@outlook.com>
2022-01-27 12:02:07 +0100

    Fix(story 10 Filter overview coaches): trying to add error handling

--- src/app/user/create-user/create-user.component.ts
+++ src/app/user/create-user/create-user.component.ts
@@ -1,6 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from "../../service/user.service";
 import {FormBuilder, FormControl, Validators} from "@angular/forms";
+import {catchError} from "rxjs";
+import {Router, RouterLink} from "@angular/router";
+import {error} from "jquery";
+import {HttpErrorResponse} from "@angular/common/http";
+import {resolve} from "@angular/compiler-cli";
+
 
 @Component({
   selector: 'app-create-user',
@@ -10,11 +16,14 @@ import {FormBuilder, FormControl, Validators} from "@angular/forms";
 export class CreateUserComponent implements OnInit {
 
   passwordRepeat?: string;
+  private isDuplicateOfExistingUser = false;
 
   constructor(
     private userService: UserService,
-    private formBuilder: FormBuilder
-  ) { }
+    private formBuilder: FormBuilder,
+    private routerLink: Router
+  ) {
+  }
 
   ngOnInit(): void {
   }
@@ -27,20 +36,29 @@ export class CreateUserComponent implements OnInit {
       [Validators.required]),
     password: new FormControl('',
       [Validators.required, Validators.pattern("^(?=.*[0-9])(?=.*[A-Z]).{8,255}$"),]),
-    email:new FormControl('',
+    email: new FormControl('',
       [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"),]),
-    company:new FormControl(''),
+    company: new FormControl(''),
   })
 
-  createUser(){
-    this.userService.createUser(this.userForm.value).subscribe();
+  createUser() {
+    this.userService.createUser(this.userForm.value).subscribe({
+      next() {
+        return true;
+      },
+      error(msg: HttpErrorResponse) {
+        console.log('Error message test' + msg);
+        resolve("");
+      }
+    });
   }
 
   onSubmit() {
-    if(this.userForm.invalid){
+    if (this.userForm.invalid) {
       this.userForm.markAllAsTouched();
     } else {
-      this.createUser();
+      console.log('value:' + this.createUser())
+      return this.createUser();
     }
   }
 
@@ -48,27 +66,27 @@ export class CreateUserComponent implements OnInit {
     this.passwordRepeat = event.target.value;
   }
 
-  validatePasswordEqualsPasswordRepeat(): boolean{
+  validatePasswordEqualsPasswordRepeat(): boolean {
     return this.passwordRepeat === this.userForm.get('password')?.value;
   }
 
-  get firstName(): FormControl{
+  get firstName(): FormControl {
     return this.userForm.get('firstName') as FormControl;
   }
 
-  get lastName(): FormControl{
+  get lastName(): FormControl {
     return this.userForm.get('lastName') as FormControl;
   }
 
-  get password(): FormControl{
+  get password(): FormControl {
     return this.userForm.get('password') as FormControl;
   }
 
-  get email(): FormControl{
+  get email(): FormControl {
     return this.userForm.get('email') as FormControl;
   }
 
-  get company(): FormControl{
+  get company(): FormControl {
     return this.userForm.get('company') as FormControl;
   }
 }
