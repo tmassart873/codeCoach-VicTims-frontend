783a43d22c71ec72be3c1fe366fe1c909b61c337
jrnsmisrt <jeroen.smissaert@gmail.com>
2022-02-02 12:16:22 +0100

    Build (mycoachprofile) if not coach of mycoachprofile user gets rerouted back to coacheeprofile

--- src/app/user/my-coach-profile/my-coach-profile.ts
+++ src/app/user/my-coach-profile/my-coach-profile.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {User} from "../model/User";
 import {UserService} from "../../service/user/user.service";
 import {ActivatedRoute, Router} from "@angular/router";
@@ -9,7 +9,7 @@ import {ActivatedRoute, Router} from "@angular/router";
   templateUrl: './coach-detail.component.html',
   styleUrls: ['./coach-detail.component.css']
 })
-export class MyCoachProfileComponent implements OnInit {
+export class MyCoachProfileComponent implements OnInit, AfterViewInit {
 
   loggedInUser!: User | null;
   color? : string;
@@ -23,10 +23,29 @@ export class MyCoachProfileComponent implements OnInit {
     this.loggedInUser = this.userService.user;
     this.color = 'waves-effect waves-light btn-large btn-floating teal lighten-3';
   }
+  ngAfterViewInit(){
+    if (!this.checkIfUserIsAuthorized()) {
+      this.router.navigate([`users/${this.userService.user?.id}/profile`])
+    }
+  }
 
   goToProfileNotLoggedIn(): void {
     this.id = this.route.snapshot.paramMap.get('id');
     console.log("id: " + this.id)
     this.router.navigate(['users/' + this.id + '/coach-profile']);
+  }
+
+  checkIfUserIsAuthorized() {
+    let urlUserId = this.route.snapshot.paramMap.get('id');
+
+    if(urlUserId!== this.userService.user?.id){
+      return false;
+    }
+    if(urlUserId === this.userService.user?.id){
+      return this.userService.user.userRole === 'COACH';
+    }
+
+    return false;
+
   }
 }
