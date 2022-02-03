b6aa9f260e3f6726bef8126e6226b5161822e09b
Dries Verreydt <johndoe@example.com>
2022-02-03 13:14:05 +0100

    fix(S4 ): hide edit button when not editable, fix for header nav init

--- src/app/user/my-profile/profile-information/user-detail.component.ts
+++ src/app/user/my-profile/profile-information/user-detail.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {UserService} from "../../../service/user/user.service";
 import {User} from "../../model/User";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-user-detail',
@@ -9,18 +10,25 @@ import {User} from "../../model/User";
 })
 export class UserDetailComponent implements OnInit {
 
-  user!: User | null;
+  user?: User | null;
   id!: string | null;
 
   @Input()
   color: string = 'waves-effect waves-light btn-large btn-floating yellow darken-2';
+  editedAble: boolean = false;
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService,
+              private activatedRoute: ActivatedRoute) {
   }
 
   ngOnInit(): void {
     this.user = this.userService.user;
-    console.log(this.user)
+    if(this.activatedRoute.parent!==null) {
+      let id = this.activatedRoute.parent!.snapshot.paramMap.get('id');
+      if(this.user?.id === id || this.user?.userRole == 'ADMIN'){
+        this.editedAble = true;
+      }
+    }
   }
 
 }
